766bef2c81a65f6c34e61119eb6363ada9709600
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-05-22 13:31:48 +0530

    add

--- urls.tsx
+++ urls.tsx
@@ -2,15 +2,15 @@
 
 https://raw.githubusercontent.com/riteshf/files/main/logo.svg
 
-https://raw.githubusercontent.com/riteshf/files/main/offer1.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/offer2.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/offer3.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/offer4.jgeg
+https://raw.githubusercontent.com/riteshf/files/main/offer1.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/offer2.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/offer3.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/offer4.jpeg
 
-https://raw.githubusercontent.com/riteshf/files/main/restaurant1.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/restaurant2.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/restaurant3.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/restaurant4.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/restaurant5.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/restaurant6.jgeg
-https://raw.githubusercontent.com/riteshf/files/main/restaurant7.jgeg
+https://raw.githubusercontent.com/riteshf/files/main/restaurant1.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/restaurant2.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/restaurant3.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/restaurant4.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/restaurant5.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/restaurant6.jpeg
+https://raw.githubusercontent.com/riteshf/files/main/restaurant7.jpeg
